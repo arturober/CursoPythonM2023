6446003685229d2d5fa742c5d0f739ddf8432245
Arturo Bernal <arturober@gmail.com>
2023-07-27 10:16:41 +0200

    clase 27/7

---
+++ Ficheros/leer_archivo.py
@@ -0,0 +1,29 @@
+# Lee el contenido de un fichero dentro de un string
+def lee_fichero():
+    with open("Ficheros/archivos/palabras.txt") as f:
+        print(f.read())    
+ 
+# Lee el contenido línea a línea forma clásica (bucle while)       
+def lee_fichero2():
+    with open("Ficheros/archivos/palabras.txt") as f:
+        linea = f.readline()
+        while linea != "":
+            print(linea.strip())
+            linea = f.readline()
+ 
+# Lee el contenido línea a línea (forma Python)           
+def lee_fichero3():
+    with open("Ficheros/archivos/palabras.txt") as f:
+        for linea in f:
+            print(linea.strip())
+    
+# Lee el contenido como una lista de strings (líneas)
+def lee_fichero4():
+    with open("Ficheros/archivos/palabras.txt") as f:
+        lineas = [linea.strip() for linea in f]
+        print(lineas)
+    
+# lee_fichero()
+# lee_fichero2()
+# lee_fichero3()
+lee_fichero4()
