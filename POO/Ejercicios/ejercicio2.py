bd7abd29de9cb06a5a1f34801d64891b4ea40bb1
Arturo Bernal <arturober@gmail.com>
2023-08-01 11:12:06 +0200

    Clase 1/8

---
+++ POO/Ejercicios/ejercicio2.py
@@ -0,0 +1,27 @@
+class Examen:
+    def __init__(self, asignatura: str, nota: float) -> None:
+        self.__asignatura = asignatura
+        self.nota = nota
+        
+    @property
+    def asignatura(self):
+        return self.__asignatura
+    
+    @property
+    def nota(self):
+        return self.__nota
+    
+    @nota.setter
+    def nota(self, nota):
+        if(nota < 0 or nota > 10):
+            raise ValueError("La nota tiene que estar entre 0 y 10")
+        self.__nota = nota
+    
+    def __repr__(self) -> str:
+        return f"{self.asignatura} ({self.nota})"
+    
+ex = Examen("Lengua", 5.75)
+# ex.asignatura = "Mates" # AttributeError: can't set attribute 'asignatura'
+ex.nota = 3
+print(ex.asignatura, ex.nota)
+# ex.nota = 13 # ValueError: La nota tiene que estar entre 0 y 10
