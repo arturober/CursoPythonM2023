77223b02a20fef5cbda9153fc3319ab6018be236
Arturo Bernal <arturober@gmail.com>
2023-09-06 09:33:57 +0200

    validar productos

--- Flask/ejemplo-productos-validacion/app.py
+++ Flask/ejemplo-productos-validacion/app.py
@@ -50,12 +50,18 @@ class Producto(db.Model):
 class ProductoSchema(Schema):
     nombre = fields.Str(
         required=True,
-        error_messages={"required": "El nombre es obligatorio"},
+        error_messages={
+            "required": "El nombre es obligatorio",
+            "type": "El campo no tiene un formato de string",
+        },
         validate=validate.Length(min=4, error="El nombre debe tener al menos 4 letras"),
     )
     precio = fields.Number(
         required=True,
-        error_messages={"required": "El precio es obligatorio"},
+        error_messages={
+            "required": "El precio es obligatorio",
+            "type": "El campo no tiene un formato numérico",
+        },
         validate=validate.Range(min=0, error="El precio no puede ser negativo"),
     )
 
